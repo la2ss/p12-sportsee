8e893a225f2970359c31f96f61d013caf3cbd93c
Lassina Sylla <lass390225@gmail.com>
2023-11-07 17:49:52 +0100

    Maj avant soutenance

--- src/index.jsx
+++ src/index.jsx
@@ -1,46 +1,22 @@
-// React
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-// Global Styles
-import './index.css'
-
-// Components
-import Header from './components/header'
-import Sidebar from './components/sidebar'
-import PagesWrapper from './components/pagesWrapper'
-
-// Pages
-
-import HomePage from './pages/home'
-import SettingsPage from './pages/settings'
-import CommunityPage from './pages/community'
-import ErrorPage from './pages/error'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 
-// Providers
-import { AppProvider } from './context'
+//CSS Styles
+import "./styles/sass/main.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AppProvider>
-            <Sidebar />
-                <Header />
-                <PagesWrapper>
+  <React.StrictMode>
+    <Router>
+      
+      <App />
+
+    </Router>
+  </React.StrictMode>
+);
 
 
-                    <Routes>
-                     
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/settings" element={<SettingsPage />} />
-                        <Route path="/community" element={<CommunityPage />} />
-                        <Route path="*" element={<ErrorPage />} />
-                    </Routes>
-                    </PagesWrapper>
-            </AppProvider>
-        </BrowserRouter>
-    </React.StrictMode>
-)
